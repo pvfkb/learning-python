c5c2f96eb63ecbbc9c574917f430dac6c3066d06
pvfkb <patrickbarker@gmail.com>
2015-11-25 19:06:07 +0000

    games and shopping cart

---
+++ shopping_cart/shopping.py
@@ -0,0 +1,32 @@
+def get_order():
+	print("[command] [item] (command is a to add, d to delete, q to quit)")
+	line = input()
+
+	command = line[:1]
+	item = line[2:]
+
+	return (command, item)
+def process_order(order, cart):
+	(command, item) = order
+
+	if command == "a":
+		cart.add(item)
+	elif command == "d" and item in cart:
+		cart.remove(item)
+	elif command == "q":
+		return False
+
+	return True
+
+def go_shopping():
+	cart = set()
+
+	while True:
+		order = get_order()
+		if not process_order(order, cart):
+			break
+
+	print(cart)
+	print("Finished!")
+
+go_shopping()
